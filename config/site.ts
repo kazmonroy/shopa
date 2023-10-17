7847c7d1b3fd5aa7ca3068a58871cfcb75d46045
kazmonroy <katherinemonroy@gmail.com>
2023-10-17 22:38:20 +0200

    Add files

---
+++ config/site.ts
@@ -0,0 +1,14 @@
+export type SiteConfig = typeof siteConfig
+
+export const siteConfig = {
+  name: "Home | Shopa",
+  description: "Shopa E-commerce ",
+  footer: [
+    { name: "Home", href: "/" },
+    { name: "About", href: "#" },
+    { name: "Terms & Conditions", href: "#" },
+    { name: "Shipping & Return Policy", href: "#" },
+    { name: "Privacy Policy", href: "#" },
+    { name: "FAQ", href: "#" },
+  ],
+}
