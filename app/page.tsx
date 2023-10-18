e57e56e586b95f4fe40135df4928c21e9162a33c
kazmonroy <katherinemonroy@gmail.com>
2023-10-18 13:31:30 +0200

    Add Products, Cart and Stripe payment

--- app/page.tsx
+++ app/page.tsx
@@ -10,19 +10,32 @@ import { ProductGrid } from "@/components/product-grid"
 import { ProductSort } from "@/components/product-sort"
 
 interface Props {
-  searchParams: { date?: string; price?: string }
+  searchParams: {
+    date?: string
+    price?: string
+    color?: string
+    category?: string
+    size?: string
+    search?: string
+  }
 }
 
 export default async function Page({ searchParams }: Props) {
-  const { date = "desc", price } = searchParams
+  const { date = "desc", price, color, category, size, search } = searchParams
   const priceOrder = price ? `| order(price ${price})` : ""
 
   const dateOrder = date ? `| order(_createdAt ${date})` : ""
-
   const order = `${priceOrder}${dateOrder}`
 
+  const productFilter = `_type == 'product'`
+  const colorFilter = color ? `&& "${color}" in colors` : ""
+  const categoryFilter = category ? `&& "${category}" in categories` : ""
+  const sizeFilter = size ? `&& "${size}" in sizes` : ""
+  const searchFilter = search ? `&& name match "${search}"` : ""
+  const filter = `*[${productFilter}${colorFilter}${categoryFilter}${sizeFilter}${searchFilter}]`
+
   const products = await client.fetch<SanityProduct[]>(
-    groq`*[_type == 'product'] ${order} {_id, _createdAt,name, sku,images, currency, price, descrption, "slug": slug.current} `
+    groq`${filter} ${order} {_id, _createdAt,name, sku,images, currency, price, descrption, "slug": slug.current} `
   )
 
   return (
--- components/site-header.tsx
+++ components/site-header.tsx
@@ -1,5 +1,6 @@
 "use client"
 
+import React from "react"
 import Link from "next/link"
 import { usePathname, useRouter, useSearchParams } from "next/navigation"
 import { Edit, ShoppingBag } from "lucide-react"
@@ -12,12 +13,26 @@ import { ThemeToggle } from "@/components/theme-toggle"
 
 export function SiteHeader() {
   const pathname = usePathname()
+  const router = useRouter()
+  const searchParams = useSearchParams()
+  const { cartCount } = useShoppingCart()
+  const defaultSearchQuery = searchParams.get("search") ?? ""
   if (pathname.startsWith("/studio")) return null
+
+  function onSubmit(event: React.SyntheticEvent<HTMLFormElement>) {
+    event.preventDefault()
+    const formData = new FormData(event.currentTarget)
+    const searchQuery = formData.get("search")
+    router.replace(`/?search=${searchQuery}`)
+  }
   return (
     <header className="sticky top-0 z-40 w-full border-b bg-background">
       <div className="mx-auto flex h-16 max-w-6xl items-center justify-between space-x-4 px-6 sm:space-x-0">
         <MainNav />
-        <form className="hidden items-center lg:inline-flex">
+        <form
+          onSubmit={onSubmit}
+          className="hidden items-center lg:inline-flex"
+        >
           <Input
             id="search"
             name="search"
@@ -25,13 +40,14 @@ export function SiteHeader() {
             autoComplete="off"
             placeholder="Search products..."
             className="h-9 lg:w-[300px]"
+            defaultValue={defaultSearchQuery}
           />
         </form>
         <div className="flex items-center space-x-1">
           <Link href="/cart">
             <Button size="sm" variant="ghost">
               <ShoppingBag className="h-5 w-5" />
-              <span className="ml-2 text-sm font-bold">0</span>
+              <span className="ml-2 text-sm font-bold">{cartCount}</span>
               <span className="sr-only">Cart</span>
             </Button>
           </Link>
