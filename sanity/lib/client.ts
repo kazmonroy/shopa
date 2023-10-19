4e4608e3dd1c6098abb55f327e80eae08511bffa
kazmonroy <katherinemonroy@gmail.com>
2023-10-19 14:08:55 +0200

    Update inventory image cloud respository

--- sanity/lib/client.ts
+++ sanity/lib/client.ts
@@ -7,6 +7,4 @@ export const client = createClient({
   dataset,
   projectId,
   useCdn,
-  token:
-    "skzYxx2FQ5bIC9WtxtWx8p7e8K0vVob1QyMwS99NKangG5ZFsv5EcaL515xgzhsxuQBfFIp5pb0zfrSEO5YDYqdI4dtOdNNffripPFsEWlDy083ddGYYQblb6etO5Xc5oqcNqAT3ulu8CIaJx3ZiLPXNyqF7CAaXRKlZevBBB3HQBdriRyAD",
 })
